e4c1227866037790f1114bce6a5b6179779b242b
M01POOL16 <M01POOL16@DESKTOP-R1QVA38>
2017-06-30 10:12:28 +0200

    Projektdateien hinzufügen.

---
+++ Trigonometrie/frmTrigonometrie.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Trigonometrie
+{
+	partial class frmTrigonometrie
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTrigonometrie));
+			this.lblβ = new System.Windows.Forms.Label();
+			this.txtBeta = new System.Windows.Forms.TextBox();
+			this.txtAlpha = new System.Windows.Forms.TextBox();
+			this.lblα = new System.Windows.Forms.Label();
+			this.txtA = new System.Windows.Forms.TextBox();
+			this.lblA = new System.Windows.Forms.Label();
+			this.txtB = new System.Windows.Forms.TextBox();
+			this.lblB = new System.Windows.Forms.Label();
+			this.txtC = new System.Windows.Forms.TextBox();
+			this.lblC = new System.Windows.Forms.Label();
+			this.btnBerechnen = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.btnReset = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// lblβ
+			// 
+			this.lblβ.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.lblβ.AutoSize = true;
+			this.lblβ.Location = new System.Drawing.Point(955, 556);
+			this.lblβ.Name = "lblβ";
+			this.lblβ.Size = new System.Drawing.Size(24, 25);
+			this.lblβ.TabIndex = 3;
+			this.lblβ.Text = "β";
+			// 
+			// txtBeta
+			// 
+			this.txtBeta.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtBeta.Location = new System.Drawing.Point(849, 553);
+			this.txtBeta.Name = "txtBeta";
+			this.txtBeta.Size = new System.Drawing.Size(100, 31);
+			this.txtBeta.TabIndex = 4;
+			// 
+			// txtAlpha
+			// 
+			this.txtAlpha.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtAlpha.Location = new System.Drawing.Point(194, 553);
+			this.txtAlpha.Name = "txtAlpha";
+			this.txtAlpha.Size = new System.Drawing.Size(100, 31);
+			this.txtAlpha.TabIndex = 6;
+			// 
+			// lblα
+			// 
+			this.lblα.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.lblα.AutoSize = true;
+			this.lblα.Location = new System.Drawing.Point(164, 556);
+			this.lblα.Name = "lblα";
+			this.lblα.Size = new System.Drawing.Size(24, 25);
+			this.lblα.TabIndex = 5;
+			this.lblα.Text = "α";
+			// 
+			// txtA
+			// 
+			this.txtA.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtA.Location = new System.Drawing.Point(911, 297);
+			this.txtA.Name = "txtA";
+			this.txtA.Size = new System.Drawing.Size(100, 31);
+			this.txtA.TabIndex = 8;
+			// 
+			// lblA
+			// 
+			this.lblA.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.lblA.AutoSize = true;
+			this.lblA.Location = new System.Drawing.Point(881, 300);
+			this.lblA.Name = "lblA";
+			this.lblA.Size = new System.Drawing.Size(24, 25);
+			this.lblA.TabIndex = 7;
+			this.lblA.Text = "a";
+			// 
+			// txtB
+			// 
+			this.txtB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtB.Location = new System.Drawing.Point(326, 294);
+			this.txtB.Name = "txtB";
+			this.txtB.Size = new System.Drawing.Size(100, 31);
+			this.txtB.TabIndex = 10;
+			// 
+			// lblB
+			// 
+			this.lblB.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.lblB.AutoSize = true;
+			this.lblB.Location = new System.Drawing.Point(296, 297);
+			this.lblB.Name = "lblB";
+			this.lblB.Size = new System.Drawing.Size(24, 25);
+			this.lblB.TabIndex = 9;
+			this.lblB.Text = "b";
+			// 
+			// txtC
+			// 
+			this.txtC.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtC.Location = new System.Drawing.Point(560, 637);
+			this.txtC.Name = "txtC";
+			this.txtC.Size = new System.Drawing.Size(100, 31);
+			this.txtC.TabIndex = 12;
+			// 
+			// lblC
+			// 
+			this.lblC.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.lblC.AutoSize = true;
+			this.lblC.Location = new System.Drawing.Point(531, 637);
+			this.lblC.Name = "lblC";
+			this.lblC.Size = new System.Drawing.Size(23, 25);
+			this.lblC.TabIndex = 11;
+			this.lblC.Text = "c";
+			// 
+			// btnBerechnen
+			// 
+			this.btnBerechnen.Location = new System.Drawing.Point(12, 12);
+			this.btnBerechnen.Name = "btnBerechnen";
+			this.btnBerechnen.Size = new System.Drawing.Size(346, 92);
+			this.btnBerechnen.TabIndex = 13;
+			this.btnBerechnen.Text = "berechnen";
+			this.btnBerechnen.UseVisualStyleBackColor = true;
+			this.btnBerechnen.Click += new System.EventHandler(this.btnBerechnen_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(1149, 784);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 14;
+			this.pictureBox1.TabStop = false;
+			// 
+			// btnReset
+			// 
+			this.btnReset.Location = new System.Drawing.Point(12, 110);
+			this.btnReset.Name = "btnReset";
+			this.btnReset.Size = new System.Drawing.Size(346, 92);
+			this.btnReset.TabIndex = 15;
+			this.btnReset.Text = "alles löschen";
+			this.btnReset.UseVisualStyleBackColor = true;
+			this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+			// 
+			// frmTrigonometrie
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(1149, 784);
+			this.Controls.Add(this.btnReset);
+			this.Controls.Add(this.btnBerechnen);
+			this.Controls.Add(this.txtC);
+			this.Controls.Add(this.lblC);
+			this.Controls.Add(this.txtB);
+			this.Controls.Add(this.lblB);
+			this.Controls.Add(this.txtA);
+			this.Controls.Add(this.lblA);
+			this.Controls.Add(this.txtAlpha);
+			this.Controls.Add(this.lblα);
+			this.Controls.Add(this.txtBeta);
+			this.Controls.Add(this.lblβ);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Name = "frmTrigonometrie";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.frmTrigonometrie_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label lblβ;
+		private System.Windows.Forms.TextBox txtBeta;
+		private System.Windows.Forms.TextBox txtAlpha;
+		private System.Windows.Forms.Label lblα;
+		private System.Windows.Forms.TextBox txtA;
+		private System.Windows.Forms.Label lblA;
+		private System.Windows.Forms.TextBox txtB;
+		private System.Windows.Forms.Label lblB;
+		private System.Windows.Forms.TextBox txtC;
+		private System.Windows.Forms.Label lblC;
+		private System.Windows.Forms.Button btnBerechnen;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button btnReset;
+	}
+}
+
---
+++ Trigonometrie/frmTrigonometrie.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trigonometrie
+{
+	public partial class frmTrigonometrie : Form
+	{
+		private double a = 0;
+		private double b = 0;
+		private double c = 0;
+		private double alpha = 0;
+		private double beta = 0;
+		private double gamma = 90;
+		private System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTrigonometrie));
+
+		public frmTrigonometrie()
+		{
+			InitializeComponent();
+		}
+
+		private void frmTrigonometrie_Load(object sender, EventArgs e)
+		{
+			this.BackgroundImage = (System.Drawing.Image)(resources.GetObject("pbDreieck.Image"));
+		}
+
+		private void btnBerechnen_Click(object sender, EventArgs e)
+		{
+			if (!Fall1())
+			{
+				Fall2();
+			}
+		}
+
+		private bool Fall1()
+		{
+			bool result = false;
+			if (!(string.IsNullOrWhiteSpace(txtA.Text)) &&
+				!(string.IsNullOrWhiteSpace(txtB.Text)))
+			{
+				berechneCfuerFall1();
+				berechneAlphafuerFall1();
+				berechneBetafuerFall1();
+				result = true;
+			}
+
+			else if (!(string.IsNullOrWhiteSpace(txtC.Text)) &&
+				!(string.IsNullOrWhiteSpace(txtB.Text)) &&
+				(string.IsNullOrWhiteSpace(txtA.Text)))
+			{
+				berechneAfuerFall1();
+				berechneAlphafuerFall1();
+				berechneBetafuerFall1();
+				result = true;
+			}
+
+			else if (!(string.IsNullOrWhiteSpace(txtC.Text)) &&
+				!(string.IsNullOrWhiteSpace(txtA.Text)) &&
+				(string.IsNullOrWhiteSpace(txtB.Text)))
+			{
+				berechneBfuerFall1();
+				berechneAlphafuerFall1();
+				berechneBetafuerFall1();
+				result = true;
+			}
+			else if (((string.IsNullOrWhiteSpace(txtA.Text)) && (string.IsNullOrWhiteSpace(txtB.Text))) ||
+				((string.IsNullOrWhiteSpace(txtB.Text)) && (string.IsNullOrWhiteSpace(txtC.Text))) ||
+				((string.IsNullOrWhiteSpace(txtA.Text)) && (string.IsNullOrWhiteSpace(txtC.Text))))
+			{
+				result = false;
+				
+			}
+			return result;
+		}
+
+		private void Fall2()
+		{
+			if (string.IsNullOrWhiteSpace(txtAlpha.Text) && !(string.IsNullOrWhiteSpace(txtBeta.Text))) 
+			{
+				berechneAlphafuerFall2();
+				berechneSeiten();
+			}
+
+			else if (string.IsNullOrWhiteSpace(txtBeta.Text)&& !(string.IsNullOrWhiteSpace(txtAlpha.Text)))
+			{
+				berechneBetafuerFall2();
+				berechneSeiten();
+			}
+			else
+			{
+				MessageBox.Show("Zooo nicht!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+		}
+
+		private void berechneCfuerFall1()
+		{
+			a = double.Parse(txtA.Text);
+			b = double.Parse(txtB.Text);
+
+			c = machsPositiv(Math.Sqrt(a * a + b * b));
+			txtC.Text = c.ToString();
+		}
+
+		private void berechneAfuerFall1()
+		{
+			c = double.Parse(txtC.Text);
+			b = double.Parse(txtB.Text);
+
+			a = machsPositiv(Math.Sqrt(c * c - b * b));
+			txtA.Text = a.ToString();
+		}
+
+		private void berechneBfuerFall1()
+		{
+			a = double.Parse(txtA.Text);
+			c = double.Parse(txtC.Text);
+
+			b = machsPositiv(Math.Sqrt(c * c - a * a));
+			txtB.Text = b.ToString();
+		}
+		private void berechneAlphafuerFall1()
+		{
+			a = double.Parse(txtA.Text);
+			c = double.Parse(txtC.Text);
+
+			alpha = machsPositiv(Math.Asin(a / c) * (360 / (2 * Math.PI)));
+			txtAlpha.Text = alpha.ToString();
+		}
+
+		private void berechneBetafuerFall1()
+		{
+			b = double.Parse(txtB.Text);
+			c = double.Parse(txtC.Text);
+
+			beta = machsPositiv(Math.Asin(b / c) * (360 / (2 * Math.PI)));
+			txtBeta.Text = beta.ToString();
+		}
+
+		private void berechneAlphafuerFall2()
+		{
+			beta = double.Parse(txtBeta.Text);
+
+			alpha = gamma - beta;
+			txtAlpha.Text = alpha.ToString();
+		}
+
+		private void berechneBetafuerFall2()
+		{
+			alpha = double.Parse(txtAlpha.Text);
+
+			beta = gamma - alpha;
+			txtBeta.Text = beta.ToString();
+
+		}
+
+		private void berechneSeiten()
+		{
+			if (!(string.IsNullOrWhiteSpace(txtA.Text)))
+			{
+				a = double.Parse(txtA.Text);
+
+				b = machsPositiv(a / Math.Tan(alpha));
+				txtB.Text = b.ToString();
+
+				c = machsPositiv(a / Math.Sin(alpha));
+				txtC.Text = c.ToString();
+			}
+
+			else if (!(string.IsNullOrWhiteSpace(txtB.Text)))
+			{
+				b = double.Parse(txtB.Text);
+
+				a = machsPositiv(Math.Tan(alpha) * b);
+				txtA.Text = a.ToString();
+
+				c = machsPositiv(b / Math.Cos(alpha));
+				txtC.Text = c.ToString();
+			}
+
+			else if (!(string.IsNullOrWhiteSpace(txtC.Text)))
+			{
+				c = double.Parse(txtC.Text);
+
+				a = machsPositiv(Math.Sin(alpha) * c);
+				txtA.Text = a.ToString();
+
+				b = machsPositiv(Math.Cos(alpha) * c);
+				txtB.Text = b.ToString();
+			}
+		}
+
+		private void btnReset_Click(object sender, EventArgs e)
+		{
+			txtA.Text = "";
+			txtB.Text = "";
+			txtC.Text = "";
+			txtAlpha.Text = "";
+			txtBeta.Text = "";
+
+			a = 0;
+			b = 0;
+			c = 0;
+			alpha = 0;
+			beta = 0;
+		}
+
+		private double machsPositiv(double zahl)
+		{
+			return Math.Sqrt(zahl * zahl);
+		}
+
+	}
+}
+
